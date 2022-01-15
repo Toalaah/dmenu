37cdbaa0cd137750cfe783f59ba15fe3adbf858f
Samuel Kunst <samuel.kunst@gmail.com>
2022-01-15 12:12:43 +0100

    add symbola font for emoji compatability when not using libxft-bgra

--- config.h
+++ config.h
@@ -5,7 +5,12 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char font[]           = "Jet Brains Mono Nerd Font:pixelsize=14:antialias=true:autohint=true";
-static const char *fonts[]        = { font, "JoyPixels:pixelsize=12:antialias=true:autohint=true"};
+// static const char *fonts[]        = { font, "JoyPixels:pixelsize=12:antialias=true:autohint=true"};
+static char *fonts[] = {
+  font,
+  "Symbola:pixelsize=15:antialias=true:autohint=true",
+  "JoyPixels:pixelsize=15:antialias=true:autohint=true",
+};
 
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
