63140662dab0465555b06425316b2ffde020d88f
Samuel Kunst <samuel.kunst@gmail.com>
2022-08-15 12:59:21 +0200

    chore: run clang-format on all source files

--- config.def.h
+++ config.def.h
@@ -1,36 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char font[] = "monospace:size=10";
-static const char *fonts[] = {
-  font,
-	"monospace:size=10"
-};
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {font, "monospace:size=10"};
+static char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
+static char selfgcolor[] = "#eeeeee";
+static char selbgcolor[] = "#005577";
 static char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {normfgcolor, normbgcolor},
+    [SchemeSel] = {selfgcolor, selbgcolor},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+    [SchemeNorm] = {OPAQUE, alpha},
+    [SchemeSel] = {OPAQUE, alpha},
+    [SchemeOut] = {OPAQUE, alpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
@@ -48,10 +45,10 @@ static const unsigned int border_width = 2;
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "normfgcolor", STRING, &normfgcolor },
-	{ "normbgcolor", STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
-	{ "prompt",      STRING, &prompt },
+    {"font", STRING, &font},
+    {"normfgcolor", STRING, &normfgcolor},
+    {"normbgcolor", STRING, &normbgcolor},
+    {"selfgcolor", STRING, &selfgcolor},
+    {"selbgcolor", STRING, &selbgcolor},
+    {"prompt", STRING, &prompt},
 };
--- config.h
+++ config.h
@@ -1,38 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[]           = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static const char *fonts[] = {
-  font,
-  "Symbola:pixelsize=15:antialias=true:autohint=true",
-  "JoyPixels:pixelsize=15:antialias=true:autohint=true",
-  "monospace:size=10"
-};
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+    font, "Symbola:pixelsize=15:antialias=true:autohint=true",
+    "JoyPixels:pixelsize=15:antialias=true:autohint=true", "monospace:size=10"};
+static char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
+static char selfgcolor[] = "#eeeeee";
+static char selbgcolor[] = "#005577";
 static char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {normfgcolor, normbgcolor},
+    [SchemeSel] = {selfgcolor, selbgcolor},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+    [SchemeNorm] = {OPAQUE, alpha},
+    [SchemeSel] = {OPAQUE, alpha},
+    [SchemeOut] = {OPAQUE, alpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
@@ -50,10 +47,7 @@ static const unsigned int border_width = 2;
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-  { "font",            STRING,  &font },
-  { "color4",          STRING,  &normfgcolor },
-  { "color0",          STRING,  &normbgcolor },
-  { "color0",          STRING,  &selfgcolor },
-  { "color4",          STRING,  &selbgcolor },
-  { "prompt",          STRING, &prompt },
+    {"font", STRING, &font},          {"color4", STRING, &normfgcolor},
+    {"color0", STRING, &normbgcolor}, {"color0", STRING, &selfgcolor},
+    {"color4", STRING, &selbgcolor},  {"prompt", STRING, &prompt},
 };
